c860cf8548e88c24dd4fd4ad1f9256f5afddc6f5
Gabriel Roberto de Abreu <gabriel.abreu@ambevtech.com.br>
2021-07-09 16:11:19 -0300

    Separating Projects and Areas

--- src/BulkyBook.MVC/Areas/Customer/Controllers/HomeController.cs
+++ src/BulkyBook.MVC/Areas/Customer/Controllers/HomeController.cs
@@ -1,22 +1,13 @@
-﻿using BulkyBook.MVC.Models;
+﻿using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace BulkyBook.MVC.Controllers
+namespace BulkyBook.MVC.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        public HomeController() { }
 
         public IActionResult Index()
         {
